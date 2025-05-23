5a69239293326744bdc3aa7f98becdc379043841
Jakub Chmielwski <kuba91821@gmail.com>
2025-05-23 17:25:25 +0200

    add czy_pierwsza func

--- projekt1.cpp
+++ projekt1.cpp
@@ -2,6 +2,18 @@
 
 using namespace std;
 
+bool czy_pierwsza(int n) {
+  if (n < 2) {
+    return false;
+  }
+  for (int i = 2; i * i <= n; ++i) {
+    if (n % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
 int main() {
 
   cout << "Hello world" << endl;
