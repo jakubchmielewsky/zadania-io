1734c76ea81dc90bd9330624f8707d959c1b126a
Jakub Chmielwski <kuba91821@gmail.com>
2025-05-23 17:11:38 +0200

    add silnia function

--- projekt1.cpp
+++ projekt1.cpp
@@ -2,9 +2,22 @@
 
 using namespace std;
 
+void silnia() {
+  unsigned int n;
+  long long silnia = 1;
+
+  cout << "Podaj n: ";
+  cin >> n;
+
+  for (int i = n; i > 1; i--)
+    silnia *= i;
+
+  cout << n << "! = " << silnia << endl;
+}
+
 int main() {
 
-  cout << "Hello world" << endl;
+  silnia();
 
   return 0;
 }
