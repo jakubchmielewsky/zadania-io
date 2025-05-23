be9e2bbd57de9364904a22b4815a9820954c3e5b
Jakub Chmielwski <kuba91821@gmail.com>
2025-05-23 17:20:58 +0200

    add menu to project

--- projekt1.cpp
+++ projekt1.cpp
@@ -2,12 +2,8 @@
 
 using namespace std;
 
-void silnia() {
-  unsigned int n;
+void silnia(unsigned int n) {
   long long silnia = 1;
-
-  cout << "Podaj n: ";
-  cin >> n;
 
   for (int i = n; i > 1; i--)
     silnia *= i;
@@ -15,9 +11,29 @@ void silnia() {
   cout << n << "! = " << silnia << endl;
 }
 
+void menu() {
+  int option;
+  cout << "[1]- oblicz silnie." << endl;
+  cin >> option;
+
+  switch (option) {
+  case 1:
+    unsigned int n;
+
+    cout << "Podaj n: ";
+    cin >> n;
+    silnia(n);
+    break;
+
+  default:
+    break;
+  }
+}
+
 int main() {
 
-  silnia();
+  int option;
+  menu();
 
   return 0;
 }
